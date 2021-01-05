62c5742d37e79bb008bba70209febff19a74b29a
iklimduman <iklimdumann@gmail.com>
2021-01-05 22:51:21 +0300

    Add audio animations

--- source.js
+++ source.js
@@ -6,48 +6,160 @@ var audio_yung = new Audio("sounds/yung.mp3");
 var audio_shaolin = new Audio("sounds/shaolin.mp3");
 
 // stop audio if its still running
-var audioArray = [audio_audioBinger,audio_ketsa,audio_growingResistance,audio_yung,audio_shaolin];
+var audioArray = [audio_audioBinger, audio_ketsa, audio_growingResistance, audio_yung, audio_shaolin];
+var activeSound;
 
-function stopAudio()
-{
-  for( var i=0 ; i<audioArray.length ; i++){
-    var isPaused = audioArray[i].paused ;
-    console.log(audioArray[i] + isPaused);
-    if( !isPaused)
-    {
-      audioArray[i].pause();
-    }
-  }
-};
+function startAudioVisualizer(){
+  document.getElementById("audio-vis").classList.add("audio-visualizer");
+  document.getElementById("audio-vis-2").classList.add("audio-visualizer-2");
+  document.getElementById("audio-vis-3").classList.add("audio-visualizer-3");
+  document.getElementById("audio-vis-4").classList.add("audio-visualizer-4");
+}
+
+function stopAudioVisualizer(){
+  document.getElementById("audio-vis").classList.remove("audio-visualizer");
+  document.getElementById("audio-vis-2").classList.remove("audio-visualizer-2");
+  document.getElementById("audio-vis-3").classList.remove("audio-visualizer-3");
+  document.getElementById("audio-vis-4").classList.remove("audio-visualizer-4");
+}
 
 //adding buttons event listeners
-document.getElementById("audioBingerButton").addEventListener("click",function()
+document.getElementById("audioBingerButton").addEventListener("click", function()
 {
-  console.log(audioArray.length) ;
-  stopAudio();
-  audio_audioBinger.play();
+  if(activeSound == audio_audioBinger)
+  {
+    if(audio_audioBinger.paused){
+      audio_audioBinger.play();
+      document.getElementById("stat-face-img").classList.add("image-border");
+      startAudioVisualizer();
+    }
+    else{
+      audio_audioBinger.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+  }
+  else{
+    if(activeSound != undefined){
+      activeSound.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+    audio_audioBinger.play();
+    document.getElementById("stat-face-img").classList.add("image-border");
+    startAudioVisualizer();
+    activeSound = audio_audioBinger;
+  }
 });
 
-document.getElementById("ketsaButton").addEventListener("click",function()
-{
-  stopAudio();
-  audio_ketsa.play();
+document.getElementById("ketsaButton").addEventListener("click", function() {
+
+  if(activeSound == audio_ketsa)
+  {
+    if(audio_ketsa.paused){
+      audio_ketsa.play();
+      document.getElementById("stat-face-img").classList.add("image-border");
+      startAudioVisualizer();
+    }
+    else{
+      audio_ketsa.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+  }
+  else{
+    if(activeSound != undefined){
+      activeSound.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+    audio_ketsa.play();
+    document.getElementById("stat-face-img").classList.add("image-border");
+    startAudioVisualizer();
+    activeSound = audio_ketsa;
+  }
+
 });
 
-document.getElementById("GrowingResistanceButton").addEventListener("click",function()
-{
-  stopAudio();
-  audio_growingResistance.play();
+document.getElementById("GrowingResistanceButton").addEventListener("click", function() {
+
+  if(activeSound == audio_growingResistance)
+  {
+    if(audio_growingResistance.paused){
+      audio_growingResistance.play();
+      document.getElementById("stat-face-img").classList.add("image-border");
+      startAudioVisualizer();
+    }
+    else{
+      audio_growingResistance.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+  }
+  else{
+    if(activeSound != undefined){
+      activeSound.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+    audio_growingResistance.play();
+    document.getElementById("stat-face-img").classList.add("image-border");
+    startAudioVisualizer();
+    activeSound = audio_growingResistance;
+  }
+
 });
 
-document.getElementById("yungButton").addEventListener("click",function()
-{
-  stopAudio();
-  audio_yung.play();
+document.getElementById("yungButton").addEventListener("click", function() {
+  if(activeSound == audio_yung)
+  {
+    if(audio_yung.paused){
+      audio_yung.play();
+      document.getElementById("stat-face-img").classList.add("image-border");
+      startAudioVisualizer();
+    }
+    else{
+      audio_yung.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+  }
+  else{
+    if(activeSound != undefined){
+      activeSound.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+    audio_yung.play();
+    document.getElementById("stat-face-img").classList.add("image-border");
+    startAudioVisualizer();
+    activeSound = audio_yung;
+  }
 });
 
-document.getElementById("shaolinButton").addEventListener("click",function()
-{
-  stopAudio();
-  audio_shaolin.play();
+document.getElementById("shaolinButton").addEventListener("click", function() {
+  if(activeSound == audio_shaolin)
+  {
+    if(audio_shaolin.paused){
+      audio_shaolin.play();
+      document.getElementById("stat-face-img").classList.add("image-border");
+      startAudioVisualizer();
+    }
+    else{
+      audio_shaolin.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+  }
+  else{
+    if(activeSound != undefined){
+      activeSound.pause();
+      document.getElementById("stat-face-img").classList.remove("image-border");
+      stopAudioVisualizer();
+    }
+    audio_shaolin.play();
+    document.getElementById("stat-face-img").classList.add("image-border");
+    startAudioVisualizer();
+    activeSound = audio_shaolin;
+  }
 });
